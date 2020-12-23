4e16bfb6db8f6b0eb76c1306b437025f52534cf7
Emre Koc <hi@emrekoc.io>
2020-12-23 15:40:26 +0700

    Add Read pages and routes

--- src/pages/Restaurants/index.js
+++ src/pages/Restaurants/index.js
@@ -1,13 +1,15 @@
 import { Route, Switch } from 'react-router-dom';
 import Edit from './Edit';
 import List from './List';
+import Read from './Read';
 
 export default function Restaurants() {
 
     return <Switch>
         <Route component={Edit} exact path="/restaurants/create"/>
         <Route component={List} exact path="/restaurants"/>
-        <Route component={Edit} exact path="/restaurants/:id"/>
+        <Route component={Read} exact path="/restaurants/:id"/>
+        <Route component={Edit} exact path="/restaurants/:id/update"/>
     </Switch>;
 
 }
