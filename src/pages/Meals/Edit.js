0102fae08464abdc327b62473c00fdc36d43bc3e
Emre Koc <hi@emrekoc.io>
2021-01-03 10:00:19 +0700

    Remove Store Select from Meals Edit

--- src/pages/Meals/Edit.js
+++ src/pages/Meals/Edit.js
@@ -12,7 +12,6 @@ export default function Edit() {
     return <Form collection="meals" doc={doc}>
         <TextField name="name"/>
         <Select collection="restaurants" name="restaurant"/>
-        <Select collection="stores" name="store"/>
         <TextField name="price" type="number"/>
     </Form>;
 
