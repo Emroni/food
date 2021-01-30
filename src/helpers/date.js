0c4d4e4c305a412a9dae17d1f301efdff250d6da
Emre Koc <hi@emrekoc.io>
2021-01-30 17:00:13 +0700

    Add week and ability to change

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,9 +1,11 @@
 import dayjs from 'dayjs';
 import isoWeek from 'dayjs/plugin/isoWeek';
 import isToday from 'dayjs/plugin/isToday';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
 
 dayjs.extend(isoWeek);
 dayjs.extend(isToday);
+dayjs.extend(weekOfYear);
 
 export function getDate(value) {
     return value ? dayjs(value) : dayjs();
--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -1,16 +1,30 @@
 import { useEffect, useState } from 'react';
-import { getStartOfWeek } from '../../helpers';
+import { getDate, getStartOfWeek } from '../../helpers';
 import { useDatabase } from '../../providers';
 import Day from './Day';
+import { Button } from '../../components';
+import clsx from 'clsx';
 
 export default function Calendar() {
 
     const [costs, setCosts] = useState(null);
+    const [date, setDate] = useState(getDate());
     const [days, setDays] = useState([]);
+    const [currentWeek, setCurrentWeek] = useState(true);
     const db = useDatabase();
 
+    function changeWeek(direction) {
+        const d = date.clone()
+            .add(direction, 'week');
+        setDate(d);
+    }
+
     useEffect(() => {
-        const start = getStartOfWeek();
+        const currentWeek = date.week() === getDate()
+            .week();
+        setCurrentWeek(currentWeek);
+
+        const start = getStartOfWeek(date);
 
         const days = new Array(7)
             .fill(0)
@@ -45,9 +59,21 @@ export default function Calendar() {
             costs.dinner += (day.meals.dinner && day.meals.dinner.price) || 0;
         });
         setCosts(costs);
-    }, [db]);
+    }, [
+        date,
+        db,
+    ]);
+
+    const weekClasses = clsx('w-16 mx-2 text-center', {
+        'underline': currentWeek,
+    });
 
     return <>
+        <div className="flex items-center justify-end mb-2">
+            <Button icon="chevron-left" onClick={() => changeWeek(-1)}/>
+            <div className={weekClasses}>Week {date.week()}</div>
+            <Button icon="chevron-right" onClick={() => changeWeek(1)}/>
+        </div>
         <div className="mb-4 text-white md:flex md:mb-2">
             <div className="bg-gray-700 flex justify-between px-2 py-1 text-right md:w-28 lg:w-36">
                 <span>Total</span>
