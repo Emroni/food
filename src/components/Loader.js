7d9ccad87e08f4131a492a9009a6aa543702527a
Emre Koc <hi@emrekoc.io>
2021-03-14 12:19:08 +0700

    Rewrite Auth and Login

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 
 export default function Loader({className}) {
 
-    const containerClasses = clsx('inline-block leading-none p-2', className);
+    const containerClasses = clsx('inline-block leading-none', className);
 
     return <div className={containerClasses}>
         <Icon className="animate-spin" name="circle-notch"/>
